f9c9a94123c53afbea2e224b374d28a2998b21ce
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2017-05-28 19:19:42 +0300

    1

---
+++ SimpleFormController_2/src/main/java/com/javainterviewpoint/SimpleFormControllerExample.java
@@ -0,0 +1,19 @@
+package com.javainterviewpoint;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.SimpleFormController;
+
+@SuppressWarnings("deprecation")
+public class SimpleFormControllerExample extends SimpleFormController
+{
+    public SimpleFormControllerExample()
+    {
+        setCommandClass(Student.class);
+        setCommandName("st");
+    }
+    @Override
+    protected ModelAndView onSubmit(Object command) throws Exception {
+        Student st = (Student)command;
+        return new ModelAndView("studentSuccess","st",st);
+    }
+}
