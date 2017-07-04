6deb21bd60dd40c7c529f2f8b223c1675b07f445
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2017-07-04 23:00:40 +0300

    1

---
+++ z3/src/main/java/model/RegistrationForm.java
@@ -0,0 +1,13 @@
+package model;
+
+public class RegistrationForm {
+    private String name = "Arthur";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
